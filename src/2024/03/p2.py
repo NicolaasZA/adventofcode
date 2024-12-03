f7ae86d4dceb8494fe765eeb63847ee387198f3c
NicolaasZA <lpretorius1995@gmail.com>
2024-12-03 16:11:19 +0200

    fix pep8 errors

--- src/2024/03/p2.py
+++ src/2024/03/p2.py
@@ -6,7 +6,7 @@ def read_commands(filename: str) -> str:
         return file.read()
 
 
-FIND_COMMANDS_REGEX = re.compile("do\(\)|don't\(\)|mul\(\d{1,3},\d{1,3}\)")
+FIND_COMMANDS_REGEX = re.compile(r"do\(\)|don\'t\(\)|mul\(\d{1,3},\d{1,3}\)")
 
 total = 0
 command = read_commands("data.txt")
@@ -23,5 +23,3 @@ for instruction in instructions:
             total += int(pieces[0]) * int(pieces[1])
 
 print(total)
-# 110561937
-# 104083373
